b8d202d38da2265627c627519d8af50213da5046
Mike Halagan <mhalagan@nmdp.org>
2016-08-27 12:32:37 -0500

    travis..

--- build.sh
+++ build.sh
@@ -9,15 +9,13 @@ sudo make install
 
 plackup -D -E deployment -s Starman -p 5000 -a bin/app.pl
 
-curl http://localhost:5000/api/v1/ars
-
 # Install java clients
 cd ../client-java
 mvn install
 
 # Install perl clients
 cd ../client-perl
-sudo perl Makefile.PL
+sudo perl Makefile.pl
 sudo make test
-
+sudo make install
 
