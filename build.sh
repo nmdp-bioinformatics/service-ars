95b1d3fa38c7997f6f444742340360ec502c2f98
Mike Halagan <mhalagan@nmdp.org>
2016-08-27 10:34:49 -0500

    Still working on travis..

--- build.sh
+++ build.sh
@@ -9,13 +9,15 @@ sudo make install
 
 plackup -E deployment -s Starman -p 3000 -a bin/app.pl &
 
+curl http://localhost:3000/api/v1/ars
+
 # Install java clients
 cd ../client-java
 mvn install
 
 # Install perl clients
 cd ../client-perl
-sudo perl Makefile.pl
+sudo perl Makefile.PL
 sudo make test
-sudo make install
+
 
